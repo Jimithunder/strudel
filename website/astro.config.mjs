cf05fa150275cd74213901207bad1832f1b4629b
MarcoAAlmeida <marcoalmeida.dev.br@gmail.com>
2025-10-11 18:09:23 -0300

    site config

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -10,8 +10,8 @@ import bundleAudioWorkletPlugin from 'vite-plugin-bundle-audioworklet';
 import tailwind from '@astrojs/tailwind';
 import AstroPWA from '@vite-pwa/astro';
 
-const site = `https://strudel.cc/`; // root url without a path
-const base = '/'; // base path of the strudel site
+const site = 'https://marcoaalmeida.codeberg.page';
+const base = '/strudel';
 const baseNoTrailing = base.endsWith('/') ? base.slice(0, -1) : base;
 
 // this rehype plugin fixes relative links
