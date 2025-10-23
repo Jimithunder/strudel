1f29550f7deb8dcf7ba5473b9b5ad87a7bdba1c1
gilfuser <gilfuser@gmail.com>
2025-10-23 10:54:38 -0300

    fix another style stuff. Boring!

--- packages/mondough/mondough.mjs
+++ packages/mondough/mondough.mjs
@@ -129,7 +129,8 @@ const mergeControls = (av, bv, safeOp) => {
 // appMethod: the applicative method to use ('appBoth', 'appLeft', or 'appRight')
 const createStructuredOp = (safeOp, appMethod) => (a, b) =>
   reify(a)
-    .fmap((av) => (bv) => mergeControls(av, bv, safeOp)) [appMethod](reify(b));
+    .fmap((av) => (bv) => mergeControls(av, bv, safeOp))
+    [appMethod](reify(b));
 
 // Register all structured operators
 const operators = ['add', 'sub', 'mul', 'div', 'mod'];
