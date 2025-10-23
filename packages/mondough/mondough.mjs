3de1976de8f67293fced69efb641d4789e03ebf6
gilfuser <gilfuser@gmail.com>
2025-10-23 11:03:52 -0300

    Prettier and eslint disagree. this is the boriest thing ever!

--- packages/mondough/mondough.mjs
+++ packages/mondough/mondough.mjs
@@ -128,9 +128,8 @@ const mergeControls = (av, bv, safeOp) => {
 // safeOp: the safe operation function (safeAdd, safeSub, etc.)
 // appMethod: the applicative method to use ('appBoth', 'appLeft', or 'appRight')
 const createStructuredOp = (safeOp, appMethod) => (a, b) =>
-  reify(a)
-    .fmap((av) => (bv) => mergeControls(av, bv, safeOp))
-    [appMethod](reify(b));
+  (reify(a)
+    .fmap((av) => (bv) => mergeControls(av, bv, safeOp)))[appMethod](reify(b));
 
 // Register all structured operators
 const operators = ['add', 'sub', 'mul', 'div', 'mod'];
