582a8752dd3f2a4bf1d238fb4b5130d6d8c7a78b
gilfuser <gilfuser@gmail.com>
2025-10-23 11:22:23 -0300

    eslint-disable no-unexpected-multiline. I feel like crying

--- packages/mondough/mondough.mjs
+++ packages/mondough/mondough.mjs
@@ -127,7 +127,7 @@ const mergeControls = (av, bv, safeOp) => {
 // Factory function to create structured operator helpers
 // safeOp: the safe operation function (safeAdd, safeSub, etc.)
 // appMethod: the applicative method to use ('appBoth', 'appLeft', or 'appRight')
-// eslint-disable-next-line no-unexpected-multiline
+/* eslint-disable no-unexpected-multiline */
 const createStructuredOp = (safeOp, appMethod) => (a, b) =>
   reify(a)
     .fmap((av) => (bv) => mergeControls(av, bv, safeOp))
